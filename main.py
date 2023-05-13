6beb9c9038227d671c40809e2d45a40daed0a544
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-14 00:00:23 +0200

    added recursive regex check loop function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-regex_string, user_string = input().split("|")
-if regex_string == "" or regex_string == user_string or (regex_string == "." and user_string != ""):
-    print("True")
-else:
-    print("False")
-    
+#Python3
+def recursive_match(regex_string, user_string):
+    if regex_string == "":
+        print(True)
+        return
+    if regex_string != "" and user_string == "":
+        print(False)
+        return
+    if regex_string[0] == user_string[0] or (
+        regex_string[0] == "." and user_string[0] != ""
+    ):
+        recursive_match(regex_string[1:], user_string[1:])
+    else:
+        print(False)
+        return
+def main():
+    regex_string, user_string = input().split("|")
+    recursive_match(regex_string, user_string)
+if __name__ == "__main__":
+    main()
