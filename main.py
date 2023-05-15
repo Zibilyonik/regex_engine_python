a4ccccaf8611d54686b7d4f7498423949aa5be4a
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-16 00:29:21 +0200

    managed to fix +, * and ? operators, struggling with combinations

--- main.py
+++ main.py
@@ -12,50 +12,61 @@ def recursive_match(regex_string, user_string, index=0):
     """
     if index == len(regex_string):
         return True
-    if regex_string[index] == ".":
-        return recursive_match(regex_string, user_string, index + 1)
-    if regex_string[index] == "?":
-        return recursive_match(regex_string, user_string, index + 1) or recursive_match(
-            regex_string, user_string[1:], index
-        )
-    if regex_string[index] == "+":
-        return recursive_match(regex_string, user_string, index + 1) or recursive_match(
-            regex_string, user_string[1:], index
-        )
-    if regex_string[index] == "*":
-        if index == 0:
-            return False
-        if regex_string[index - 1] == ".":
+    if index + 1 != len(regex_string):
+        if regex_string[index + 1:index + 2] == "*":
+            star_letter = regex_string[index]
+            regex_string = regex_string[:index] + regex_string[index+2:]
+            if star_letter == ".":
+                return True
+            while user_string[index] == star_letter:
+                user_string = user_string[:index] + user_string[index+1:]
+                if index == len(user_string):
+                    return True
             return recursive_match(regex_string, user_string, index + 1)
-        if regex_string[index - 1] == user_string[index - 1]:
+        if regex_string[index + 1:index + 2] == "+":
+            plus_letter = regex_string[index]
+            regex_string = regex_string[:index] + regex_string[index+2:]
+            if plus_letter != user_string[index] and plus_letter != ".":
+                return False
+            if plus_letter == "." and index != len(user_string) - 1:
+                return True
+            while user_string[index] == plus_letter:
+                user_string = user_string[:index] + user_string[index+1:]
+                if index == len(user_string):
+                    return True
             return recursive_match(regex_string, user_string, index + 1)
-        return recursive_match(regex_string, user_string[1:], index)
-    if regex_string[index] == user_string[index]:
+        if regex_string[index + 1: index + 2] == "?":
+            if regex_string[index] == user_string[index]:
+                regex_string = regex_string[:index+1] + regex_string[index+2:]
+                return recursive_match(regex_string, user_string, index + 1)
+            regex_string = regex_string[:index] + regex_string[index+2:]
+            return recursive_match(regex_string, user_string, index)
+    if regex_string[index] == user_string[index] or regex_string[index] == ".":
         return recursive_match(regex_string, user_string, index + 1)
     return False
 
 
 def regex_check(regex_string, user_string):
     if regex_string[0] == "^" and regex_string[-1] != "$":
-        if recursive_match(regex_string, user_string[: len(regex_string)], 1):
-            return True
-        return False
+        regex_string = regex_string[1:]
+        user_string = user_string[: len(regex_string)]
+        return recursive_match(regex_string, user_string)
     if regex_string[-1] == "$" and regex_string[0] != "^":
         regex_string = regex_string[:-1]
         user_string = user_string[-len(regex_string) :]
-        if recursive_match(regex_string, user_string):
-            return True
-        return False
+        return recursive_match(regex_string, user_string)
     if regex_string[0] == "^" and regex_string[-1] == "$":
         regex_string = regex_string[1:-1]
-        if len(regex_string) != len(user_string):
+        if len(regex_string) != len(user_string) and ["*","?","+"] not in regex_string:
             return False
-        if recursive_match(regex_string, user_string):
-            return True
+        return recursive_match(regex_string, user_string)
+    if len(regex_string) != len(user_string):
+        for i in range(len(user_string)):
+            if recursive_match(regex_string, user_string[i:]):
+                return True
         return False
-    for i in range(len(user_string)):
-        if recursive_match(regex_string, user_string, i):
-            return True
+    else:
+        return recursive_match(regex_string, user_string)
 
 
 def main():
