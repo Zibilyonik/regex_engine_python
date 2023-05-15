75e51b4bb2313251178bae4e84bd79d7b8306939
Onur Surmeli <onatsurmeli@gmail.com>
2023-05-15 23:03:35 +0200

    added +, * and ? operator functionality

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
-#Python3
-def recursive_match(regex_string, user_string):
+# Python3
+def recursive_match(regex_string, user_string, index=0):
     """Match a regex string to a user string.
 
     Args:
         regex_string (string): The regex string to match.
         user_string (string): The user string to match.
+        index (int, optional): The index of the regex string to match. Defaults to 0.
 
     Returns:
         bool: True if the regex string matches the user string, False otherwise.
     """
-    if regex_string == "":
+    if index == len(regex_string):
         return True
-    if regex_string != "" and user_string == "":
-        return False
-    if regex_string[0] == user_string[0] or (
-        regex_string[0] == "." and user_string[0] != ""
-    ):
-        if recursive_match(regex_string[1:], user_string[1:]):
-            return True
-    else:
-        return False
+    if regex_string[index] == ".":
+        return recursive_match(regex_string, user_string, index + 1)
+    if regex_string[index] == "?":
+        return recursive_match(regex_string, user_string, index + 1) or recursive_match(
+            regex_string, user_string[1:], index
+        )
+    if regex_string[index] == "+":
+        return recursive_match(regex_string, user_string, index + 1) or recursive_match(
+            regex_string, user_string[1:], index
+        )
+    if regex_string[index] == "*":
+        if index == 0:
+            return False
+        if regex_string[index - 1] == ".":
+            return recursive_match(regex_string, user_string, index + 1)
+        if regex_string[index - 1] == user_string[index - 1]:
+            return recursive_match(regex_string, user_string, index + 1)
+        return recursive_match(regex_string, user_string[1:], index)
+    if regex_string[index] == user_string[index]:
+        return recursive_match(regex_string, user_string, index + 1)
+    return False
+
 
 def regex_check(regex_string, user_string):
     if regex_string[0] == "^" and regex_string[-1] != "$":
-        regex_string = regex_string[1:]
-        if recursive_match(regex_string, user_string[:len(regex_string)]):
+        if recursive_match(regex_string, user_string[: len(regex_string)], 1):
             return True
         return False
     if regex_string[-1] == "$" and regex_string[0] != "^":
         regex_string = regex_string[:-1]
-        user_string = user_string[-len(regex_string):]
+        user_string = user_string[-len(regex_string) :]
         if recursive_match(regex_string, user_string):
             return True
         return False
@@ -41,9 +54,9 @@ def regex_check(regex_string, user_string):
             return True
         return False
     for i in range(len(user_string)):
-        if recursive_match(regex_string, user_string):
+        if recursive_match(regex_string, user_string, i):
             return True
-        user_string = user_string[i:]
+
 
 def main():
     regex_string, user_string = input().split("|")
@@ -53,7 +66,8 @@ def main():
     if regex_string != "" and user_string == "":
         print(False)
         return
-    print(regex_check(regex_string, user_string))  
+    print(regex_check(regex_string, user_string))
+
 
 if __name__ == "__main__":
     main()
